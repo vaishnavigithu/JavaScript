382458c435ea4f7fa65bb95e2365dc89ee9d5dcf
Vaishnavi <110771907+vaishnavigithu@users.noreply.github.com>
2022-09-21 23:35:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,228 @@
+/*
+var x=2;
+console.log(x);
+x=5;
+console.log(x);
+var x=8;
+console.log(x);
+
+
+function test2(){
+    var y=9;
+    x=1;
+}
+
+if(true){
+    var y=1;
+   // console.log(y); var is global variable can be assessed anywhere in the code. (function scope)
+}
+console.log(y);
+
+//Hoisting 
+
+console.log(d);
+var d=12;
+*/
+// 2.let - block scope- local variable
+ /* 
+let v=13;
+console.log(v);
+
+if(true){
+    let var2=15;
+    //console.log(var2);
+
+}
+
+console.log(var2);
+
+*/
+//3.const
+
+//const variable4=2;
+ //console.log(variable4);
+
+
+ /*const myarr=[1,2,3,4,"vaish"];
+ console.log(myarr);
+ console.log(myarr.length);
+ myarr[2]=19;
+ console.log(myarr);
+ myarr[4]="devu";
+ console.log(myarr);
+
+ const myObject={
+    'key': 'value',
+    'key1':'value2',
+    'key2':{
+        'key3':[1,2,3],
+        'key4':5
+    }
+
+ };
+ console.log(myObject);
+ console.log(myObject['key2']);
+ console.log(myObject['key1']);
+
+
+var var5 = document.getElementById("eg");
+console.log(var5);
+*/
+//Data types in jS
+//1. Number
+// var number=10;
+// var number1=20;
+// console.log(number+number1);
+// var num1=455;
+// var num2=45.12;
+// //2.string
+// var str1="this is string";
+// var str2="this is also string";
+
+// objects
+/*
+var marks={
+    ravi:12,
+    devu:90,
+    vaishnavi:85.98
+
+}
+console.log(marks);
+
+//Boolean
+
+var a=true;
+var b=false;
+console.log(a);
+console.log(b);
+//undefined
+var var1;
+console.log(var1);
+*/
+//arithmatic operators
+/*
+var a=100;
+var b=10;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
+//Assignment operatoes
+var c=b;
+c+=2;
+c-=5;
+c*=2;
+c/=2;
+console.log(c);
+*/
+// var x=20;
+// var y=50;
+// console.log(x==y);  //false
+// console.log(x>y);    //false
+// console.log(x<y);   //true
+// console.log(x>=y);  //false
+// console.log(x<=y);    //true
+
+//logical operator
+
+//logical and
+// console.log(true && true);  //true
+// console.log(true && false);   //false
+// console.log(false && true);     //false
+// console.log(false && false);     //false
+
+//logical or
+// console.log(true || true);  //t
+// console.log(true || false);  //t
+// console.log(false || true);   //t
+// console.log(false || false);  //f
+
+//logical not
+// console.log(!true);
+// console.log(!false);
+
+// function avg(a,b){
+//     return (a+b)/2;
+// }
+
+// c=avg(5,10);   //calling function
+// c1=avg(10,20);
+// console.log(c,c1);
+
+// var age=50;
+// if(age<15){
+//     console.log("Im vaish");
+// }
+// else if(age>10){
+//     console.log("im devika");
+// }
+// else if(age==10){
+//     console.log("im devika todkari");
+// }
+// else{
+//     console.log("im pawar");
+
+// }
+                    //FOR loop
+// var myArray=[1,2,3,"good","bye"];
+// console.log(myArray);
+// for(var i=0;i<myArray.length;i++){
+//     if(i==2){
+//         break;
+//     }
+//     console.log(myArray[i]);
+// }
+// for(var i=0;i<myArray.length;i++){
+//     if(i==2){
+//         continue;
+//     }
+//     console.log(myArray[i]);
+// }
+
+// myArray.forEach(function(element){
+//     console.log(element);
+// });
+                //while loop
+// let j=0;
+// while(j<myArray.length){
+//    console.log(myArray[j]);
+//    j++;
+// }
+
+// let myarr=["vaish","devu",90,null,true];
+// console.log(myarr.length); 
+// myarr.pop();  //last element pop
+// myarr.push("frontend");   //added at last
+// myarr.shift();    //1st element removed
+// myarr.unshift("om");   //added at 1st
+// console.log(myarr);
+
+// let mystring = "vaish is a good girl girl";
+// console.log(mystring.length);
+// console.log(mystring.indexOf("girl"));
+// console.log(mystring.lastIndexOf("girl"));
+
+// console.log(mystring.slice(0,3));
+// d = mystring.replace("vaish","devu");
+// console.log(d);
+
+
+// let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.getTime());   //gives time in seconds
+// console.log(myDate.getFullYear());
+// console.log(myDate.getDate());
+// console.log(myDate.getDay());
+// console.log(myDate.getHours());
+// console.log(myDate.getMonth());
+
+
+// document.getElementById('click').click()
+// let ele=document.getElementsByClassName("container");
+// console.log(ele);
+// ele[0].style.background= "aqua";
+
+
+
+
